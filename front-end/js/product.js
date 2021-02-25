3efca22dea78b78a14d0be44f2b7a995df4e3bc4
Florent Feugere <florent.feugere@gmail.com>
2021-02-25 11:37:33 +0100

    Page produit finie

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -17,67 +17,45 @@ function fetchDataProduct(){
     .then(reponse => reponse.json())
     .then((dataProduct) => {
         console.log(dataProduct);
-        document.querySelector(".details").innerHTML +=`
-        <img src="${dataProduct.imageUrl}" class="details__img" alt="" />
-        <div class="details__name">
-        <h2>${dataProduct.name}</h2>
-        </div>
-        <div class="details__text">
-        <p>
-        ${dataProduct.description}
-      </p>
-        </div>
-        <div class="details__options">
-      <label for="lense-select">Choisissez une lentille:</label>
-      <select class="options__select" name="lenses" id="lense-select" required>
-        <option value="">--Choisissez une option--</option>
-        <option value="1">${dataProduct.lenses[0]}</option>
-        <option value="2">${dataProduct.lenses[1]}</option>
-      </select>
-    </div>
-    <div class="details__price">
-      <p><strong>${dataProduct.price/100}</strong> €</p>
-    </div>
-    <button class="details__btn" name="Ajouter au panier" type="submit">
-      Ajouter au panier
-    </button>
-        `; 
+        displayProduct(dataProduct);
     })
 }
+
+//Fonction qui prend le résultat de la requête GET produit et l'affiche sur la page
+function displayProduct(data){
+  document.querySelector(".details").innerHTML +=`
+          <img src="${data.imageUrl}" class="details__img" alt="" />
+          <div class="details__name">
+            <h2>${data.name}</h2>
+          </div>
+          <div class="details__text">
+            <p>
+              ${data.description}
+            </p>
+          </div>
+          <div class="details__options">
+            <label for="lense-select">Choisissez une lentille:</label>
+            <select class="options__select" name="lenses" id="lense-select" required>
+                <option value="">--Choisissez une option--</option>
+            </select>
+          </div>
+          <div class="details__price">
+            <p>
+              <strong>${data.price/100}</strong> €</p>
+          </div>
+          <button class="details__btn" name="Ajouter au panier" type="submit">
+            Ajouter au panier
+          </button>
+          `;
+
+  // Ajouter le contenu de l'array lenses dans le menu select
+
+  for (let i = 0; i < data.lenses.length ; i++ ){
+    const option = document.createElement("option"); //création de <option>
+    option.value += data.lenses.indexOf(`${data.lenses[i]}`); // ça récupère l'index du tableau 
+    option.innerHTML += `${data.lenses[i]}`; // le nom de la lentille est ajouté à <option> 
+    document.getElementById("lense-select").appendChild(option); //ça ajoute <option> en enfant de l'id #lense-select 
+  };
+};
+
 fetchDataProduct();
-
-
-// //Prendre les résultats du fetch et les mettre dans mon HTML
-// function displayProduct(){
-//     document.querySelector(".details").innerHTML +=`
-//     <img src="${imageUrl}" class="details__img" alt="" />
-//     <div class="details__name">
-//       <h2>XXX XX XX</h2>
-//     </div>
-//     <div class="details__text">
-//       <p>
-//         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-//         ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//         aliquip ex ea commodo consequat. Duis aute irure dolor in
-//         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-//         pariatur.
-//       </p>
-//     </div>
-//     <div class="details__options">
-//       <label for="lense-select">Choisissez une lentille:</label>
-//       <select class="options__select" name="lenses" id="lense-select" required>
-//         <option value="">--Choisissez une option--</option>
-//         <option value="1">Lentille 1</option>
-//         <option value="2">Lentille 2</option>
-//       </select>
-//     </div>
-//     <div class="details__price">
-//       <p><strong>506</strong>€</p>
-//     </div>
-//     <button class="details__btn" name="Ajouter au panier" type="submit">
-//       Ajouter au panier
-//     </button>
-//     `;
-
-// }
